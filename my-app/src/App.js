d86235ed0a6736d3f9939013b91ce32414edcf64
RangiBecerra <rangibecerra@gmail.com>
2017-11-20 19:59:58 -0300

    trabajo en login

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 // importamos que vamos a utilizar de bootstrap
 // Debemos escribir los componentes de bootstrap
-import {  } from 'react-bootstrap'
+import { FormGroup, FormControl} from 'react-bootstrap'
 
+class Login extends Component {
+	render() {
+		return (
+			<form>
+			    <FormGroup bsSize="large">
+			      <FormControl type="text" placeholder="Large text" />
+			    </FormGroup>
+			    <FormGroup>
+			      <FormControl type="text" placeholder="Normal text" />
+			    </FormGroup>
+			    <FormGroup bsSize="small">
+			      <FormControl type="text" placeholder="Small text" />
+			    </FormGroup>
+			</form>
+		)
+	}
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      texto
+      <Login/>
       </div>
     );
   }
