616331d1899b33ec4e4d0b6b4acc15f148fd0c2a
RangiBecerra <rangibecerra@gmail.com>
2017-11-20 19:30:18 -0300

    practicando

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 // importamos que vamos a utilizar de bootstrap
+// Debemos escribir los componentes de bootstrap
 import { Button } from 'react-bootstrap'
 
 class ButtonExample extends Component {
+
   render() {
     return (
       <Button bsSize="large">Botón de prueba</Button>
