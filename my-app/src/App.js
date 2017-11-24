93f722ff7a87aef263a78d67345329285a946cd4
RangiBecerra <rangibecerra@gmail.com>
2017-11-23 21:05:36 -0300

    css de login

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,27 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 // importamos que vamos a utilizar de bootstrap
 // Debemos escribir los componentes de bootstrap
-import { FormGroup, FormControl, Button, Grid, Row, Col, Radio} from 'react-bootstrap'
+import { FormGroup, FormControl, Button, Grid, Row, Col, Radio, Image} from 'react-bootstrap'
 
 class Login extends Component {
 	render() {
 		return (
 			<Grid className="Login">
     			<Row className="show-grid">
-      				<Col xs={6} xsOffset={3}>
-      					<code>
+    				<Col xs={6} xsOffset={3}>
+    					<Image className="Brand" src="img/brand.png" responsive />
+    				</Col>
+      				<Col xs={10} xsOffset={1}>
 							<form>
 							    <FormGroup>
-							      <FormControl type="text" placeholder="Email ID"/>
+							    	<FormControl type="text" placeholder="Email ID"/>
 							    </FormGroup>
 							    <FormGroup>
-							      <FormControl type="text" placeholder="Password"/>
+							    	<FormControl type="text" placeholder="Password"/>
 							    </FormGroup>
-							    <Button bsSize="large" block>Block level button</Button>
-							    <Radio checked readOnly>Recordar Mi Usuario</Radio>
-							    <p>¿Olvidaste tus Datos?</p>
+							    	<Button bsStyle="primary" block><Image src="img/sbee.png"/></Button>
+							    <Col xs={6}>
+							    	<Radio checked readOnly className="Logintext">Recordar Mi Usuario</Radio>
+							    </Col>
+							    <Col xs={6}>
+							    	<p className="Logintext">¿Olvidaste tus Datos?</p>
+							    </Col>
 							</form>
-						</code>
 					</Col>
 				</Row>
 			</Grid>
