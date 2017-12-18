6cf3789f8ad3d0353502ad493c6e45f76ec4b804
RangiBecerra <rangibecerra@gmail.com>
2017-12-18 19:22:01 -0300

    Updates

--- my-app/src/actions/types.js
+++
@@ -1,4 +0,0 @@
-// Carga de apiaries
-export const LOAD_APIARIES = 'LOAD_APIARIES';
-
-export const EDIT_APIARY_STATUS = 'EDIT_APIARY_STATUS';
--- my-app/src/reducers/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import apiaryReducer from './apiaryReducer';
-// Combinar todos los reducers en uno solo
-import {combineReducers} from 'redux';
-
-const rootReducer = combineReducers ({
-  apiaries : apiaryReducer
-})
-
-export default rootReducer;
